98fe7d4db560563873dad260cab6ba53022acd68
jalamis <jalamis@remax.com>
2024-11-30 20:47:11 -0600

    initial

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// Create basic HTML elements
+const app = document.createElement("div");
+app.id = "app";
+app.innerHTML = `
+  <h1>Welcome to CB Front</h1>
+  <p>This is a basic HTML setup</p>
+`;
+
+// Add the app div to the document body
+document.body.appendChild(app);
